6253623e900827926dddec07b17b92403ff46239
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-25 20:12:51 +0200

    add-automation-Ttest-Finished

--- images_code.py
+++ images_code.py
@@ -3,9 +3,10 @@ import os
 
 file_size = int(input('Enter size: '))
 output_folder = input('Enter output_folder Name: ')
+
 os.chdir('images')
 
-if not os.path.exists(output_folder):  # python create folder if not exists
+if not os.path.isdir(output_folder):  # python create folder if not exists
    os.mkdir(output_folder)
 
 for filename in os.listdir('.'):    #python code loop in folder file
@@ -14,7 +15,7 @@ for filename in os.listdir('.'):    #python code loop in folder file
         image= Image.open(filename)
         width , heigth = image.size    #python pillow get image width and height
         #print(width , heigth)
-        '''if width > file_size and heigth > file_size:
+        if width > file_size and heigth > file_size:
             if width > heigth:
                 heigth= int((file_size/width)*heigth)
                 width= file_size
@@ -23,6 +24,6 @@ for filename in os.listdir('.'):    #python code loop in folder file
                 width= int((file_size/heigth)*width)
                 heigth= file_size
             image = image.resize((width,heigth))
-            image = image.save(output_folder,filename)'''
-        image = image.resize((file_size,file_size))
-        image = image.save(output_folder,filename.lower())
+            image = image.save(os.path.join(output_folder,filename.lower()))
+            #image = image.resize((file_size,file_size))
+            #image = image.save(output_folder,filename.lower())
